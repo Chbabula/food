1abe22ee2faffb1d20d7d464f62c9a5ec3b95c46
Chbabula <chinthanippulababulal@gmail.com>
2024-02-15 18:05:59 +0530

    debugging

--- food/urls.py
+++ food/urls.py
@@ -1,57 +1,29 @@
-"""
-URL configuration for food project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from .views import OrganizationViewSet, ItemViewSet, PricingViewSet, home, CalculateDeliveryCost
 from rest_framework.routers import DefaultRouter
 from django.urls import include, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.views.generic.base import RedirectView
-
 
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Food Delivery Price Calculator API",
-      default_version='v1',
-      description="Food Delivery Price Calculator API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+schema_view = get_swagger_view(title='Food Delivery Price Calculator API')
+
+# urlpatterns = [
+#     re_path(r'^$', schema_view)
+# ]
 
 router = DefaultRouter()
 router.register(r'organizations', OrganizationViewSet, basename= 'organization')
 router.register(r'items', ItemViewSet, basename='item')
 router.register(r'pricing', PricingViewSet, basename='pricing')
+router.register(r'calculate_delivery_cost', CalculateDeliveryCost, basename='calculate_delivery_cost')
+
 
 urlpatterns = [
-    # re_path(r'^$', schema_view),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^$', schema_view),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/calculate_delivery_cost/', CalculateDeliveryCost.as_view(), name='calculate food delivery cost'),
-    re_path(r'^.*$', RedirectView.as_view(url='swagger/', permanent=False)),
+    path('api/calculate_delivery_cost/', CalculateDeliveryCost.as_view(), name='calculate_delivery_cost'),
+
 ]
